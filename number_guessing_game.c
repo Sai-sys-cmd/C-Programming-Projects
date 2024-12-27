29defea64dc45191096f5d0e940265b8cfa7dc26
Sai-sys-cmd <s300038602@ddsbstudent.ca>
2024-12-27 16:51:54 -0500

    Update number_guessing_game.c

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -7,30 +7,42 @@ void play_game(int lower, int upper, int max_guesses)
     int guess, number, attempts = 0;
     number = rand() % (upper - lower + 1) + lower;
 
-    printf("I'm thinking of a number between %d and %d. Try to guess it in %d attempts!\n", lower, upper, max_guesses);
+    printf("\n--- Game Started ---\n");
+    printf("I'm thinking of a number between %d and %d.\n", lower, upper);
+    printf("Try to guess it in %d attempts!\n", max_guesses);
+    printf("---------------------\n");
 
-    while (attempts < max_guesses) {
-        printf("Enter your guess: ");
-        if (scanf("%d", &guess) != 1) {
-            printf("Please enter a valid number.\n");
+    while (attempts < max_guesses) 
+    {
+        printf("\nEnter your guess: ");
+        
+        if (scanf("%d", &guess) != 1) 
+        {
+            printf("Invalid input! Please enter a valid number.\n");
             while (getchar() != '\n');
             continue;
         }
+
         attempts++;
 
-        if (guess > number) {
+        if (guess > number) 
+        {
             printf("Too high! Try again.\n");
-        } else if (guess < number) {
+        } 
+        else if (guess < number) 
+        {
             printf("Too low! Try again.\n");
-        } else {
-            printf("Correct! You guessed the number in %d attempts.\n", attempts);
+        } 
+        else 
+        {
+            printf("\nCorrect! You guessed the number in %d attempts.\n", attempts);
             return;
         }
 
         printf("You have %d guesses left.\n", max_guesses - attempts);
     }
 
-    printf("Sorry, you've run out of guesses! The correct number was %d.\n", number);
+    printf("\nSorry, you've run out of guesses! The correct number was %d.\n", number);
 }
 
 int main() 
@@ -40,8 +52,9 @@ int main()
 
     srand(time(0));
 
-    do {
-        printf("\nWelcome to the Number Guessing Game!\n");
+    do 
+    {
+        printf("\n==== Welcome to the Number Guessing Game! ====\n");
         printf("Choose a difficulty level:\n");
         printf("1. Easy (1 to 10)\n");
         printf("2. Medium (1 to 100)\n");
@@ -49,61 +62,83 @@ int main()
         printf("4. Very Hard (1 to 10000)\n");
         printf("5. Extreme (1 to 100000)\n");
         printf("6. Insane (1 to 1000000)\n");
+        printf("--------------------------------------------\n");
 
-        while (1) {
-            printf("Enter your choice: ");
-            if (scanf("%d", &choice) != 1) {
-                printf("Invalid input! Please enter 1, 2, 3, 4, 5, or 6.\n");
+        while (1) 
+        {
+            printf("Enter your choice (1-6): ");
+            
+            if (scanf("%d", &choice) != 1) 
+            {
+                printf("Invalid input! Please enter a number between 1 and 6.\n");
                 while (getchar() != '\n');
                 continue;
             }
 
-            if (choice == 1) {
+            if (choice == 1) 
+            {
                 lower = 1;
                 upper = 10;
                 max_guesses = 5;
                 break;
-            } else if (choice == 2) {
+            } 
+            else if (choice == 2) 
+            {
                 lower = 1;
                 upper = 100;
                 max_guesses = 7;
                 break;
-            } else if (choice == 3) {
+            } 
+            else if (choice == 3) 
+            {
                 lower = 1;
                 upper = 1000;
                 max_guesses = 10;
                 break;
-            } else if (choice == 4) {
+            } 
+            else if (choice == 4) 
+            {
                 lower = 1;
                 upper = 10000;
                 max_guesses = 12;
                 break;
-            } else if (choice == 5) {
+            } 
+            else if (choice == 5) 
+            {
                 lower = 1;
                 upper = 100000;
                 max_guesses = 15;
                 break;
-            } else if (choice == 6) {
+            } 
+            else if (choice == 6) 
+            {
                 lower = 1;
                 upper = 1000000;
                 max_guesses = 20;
                 break;
-            } else {
-                printf("Invalid choice! Please enter 1, 2, 3, 4, 5, or 6.\n");
+            } 
+            else 
+            {
+                printf("Invalid choice! Please enter a valid number between 1 and 6.\n");
                 while (getchar() != '\n');
             }
         }
 
         play_game(lower, upper, max_guesses);
 
-        printf("\nDo you want to play again? (1 for Yes, 0 for No): ");
-        if (scanf("%d", &play_again) != 1) {
+        printf("\n--------------------------------------------\n");
+        printf("Do you want to play again? (1 for Yes, 0 for No): ");
+        
+        if (scanf("%d", &play_again) != 1) 
+        {
             printf("Invalid input! Exiting...\n");
             break;
         }
-    } while (play_again == 1);
 
-    printf("Thanks for playing! Goodbye.\n");
+    } 
+    while (play_again == 1);
+
+    printf("\n==== Thanks for playing! Goodbye! ====\n");
 
     return 0;
 }
