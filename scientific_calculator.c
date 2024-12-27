7243b573425bedd9f1b6c942854ffb389d1ae7d5
Sai-sys-cmd <s300038602@ddsbstudent.ca>
2024-12-27 16:54:34 -0500

    Update scientific_calculator.c

--- scientific_calculator.c
+++ scientific_calculator.c
@@ -6,8 +6,8 @@ int main() {
     double num1, num2;
     int valid_input;
 
-    while(1) {
-        printf("\nScientific Calculator\n");
+    while (1) {
+        printf("\n==== Scientific Calculator ====\n");
         printf("1. Add\n");
         printf("2. Subtract\n");
         printf("3. Multiply\n");
@@ -18,146 +18,165 @@ int main() {
         printf("8. Logarithm\n");
         printf("9. Trigonometry\n");
         printf("10. Exit\n");
+        printf("-------------------------------\n");
 
-        printf("Select an option: ");
+        printf("Select an option (1-10): ");
         valid_input = scanf("%d", &choice);
 
         if (valid_input != 1) {
             printf("Invalid option. Please enter a number between 1 and 10.\n");
-            while(getchar() != '\n');
+            while (getchar() != '\n');
             continue;
         }
 
-        switch(choice) {
+        switch (choice) {
+
             case 1:
-                printf("Enter two numbers: ");
+                printf("\nEnter two numbers: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
                 printf("Result: %.2lf\n", num1 + num2);
                 break;
-                
+
             case 2:
-                printf("Enter two numbers: ");
+                printf("\nEnter two numbers: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
                 printf("Result: %.2lf\n", num1 - num2);
                 break;
-                
+
             case 3:
-                printf("Enter two numbers: ");
+                printf("\nEnter two numbers: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
                 printf("Result: %.2lf\n", num1 * num2);
                 break;
-                
+
             case 4:
-                printf("Enter two numbers: ");
+                printf("\nEnter two numbers: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
-                if (num2 != 0)
+                if (num2 != 0) {
                     printf("Result: %.2lf\n", num1 / num2);
-                else
+                } else {
                     printf("Can't divide by zero!\n");
+                }
                 break;
-                
+
             case 5:
-                printf("Enter two integers: ");
+                printf("\nEnter two integers: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
                 printf("Result: %.0lf\n", (int)num1 % (int)num2);
                 break;
-                
+
             case 6:
-                printf("Enter a number: ");
+                printf("\nEnter a number: ");
                 valid_input = scanf("%lf", &num1);
                 if (valid_input != 1) {
                     printf("Invalid input. Please enter a valid number.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
-                if (num1 >= 0)
+                if (num1 >= 0) {
                     printf("Result: %.2lf\n", sqrt(num1));
-                else
+                } else {
                     printf("Can't find the square root of a negative number.\n");
+                }
                 break;
-                
+
             case 7:
-                printf("Enter base and exponent: ");
+                printf("\nEnter base and exponent: ");
                 valid_input = scanf("%lf %lf", &num1, &num2);
                 if (valid_input != 2) {
                     printf("Invalid input. Please enter two numbers.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
                 printf("Result: %.2lf\n", pow(num1, num2));
                 break;
-                
+
             case 8:
-                printf("Enter a number: ");
+                printf("\nEnter a number: ");
                 valid_input = scanf("%lf", &num1);
                 if (valid_input != 1) {
                     printf("Invalid input. Please enter a valid number.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
-                if (num1 > 0)
+                if (num1 > 0) {
                     printf("Result: %.2lf\n", log(num1));
-                else
+                } else {
                     printf("Can't find log of non-positive numbers.\n");
+                }
                 break;
-                
+
             case 9:
-                printf("Select trigonometric function (1: Sin, 2: Cos, 3: Tan): ");
+                printf("\nSelect trigonometric function:\n");
+                printf("1. Sin\n");
+                printf("2. Cos\n");
+                printf("3. Tan\n");
+                printf("----------------------------\n");
+
                 int trig_choice;
                 valid_input = scanf("%d", &trig_choice);
                 if (valid_input != 1) {
                     printf("Invalid choice. Please enter 1, 2, or 3.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
+
                 printf("Enter an angle in radians: ");
                 valid_input = scanf("%lf", &num1);
                 if (valid_input != 1) {
                     printf("Invalid input. Please enter a valid number.\n");
-                    while(getchar() != '\n');
+                    while (getchar() != '\n');
                     break;
                 }
-                if (trig_choice == 1)
+
+                if (trig_choice == 1) {
                     printf("Sin: %.2lf\n", sin(num1));
-                else if (trig_choice == 2)
+                } 
+                else if (trig_choice == 2) {
                     printf("Cos: %.2lf\n", cos(num1));
-                else if (trig_choice == 3)
+                } 
+                else if (trig_choice == 3) {
                     printf("Tan: %.2lf\n", tan(num1));
-                else
+                } 
+                else {
                     printf("Invalid trigonometric function.\n");
+                }
                 break;
-                
+
             case 10:
-                printf("Goodbye!\n");
+                printf("\nGoodbye!\n");
                 return 0;
+
             default:
-                printf("Invalid option. Please select a valid option.\n");
+                printf("Invalid option. Please select a valid option (1-10).\n");
         }
+
+        printf("\n-----------------------------------------\n");
     }
 
     return 0;
